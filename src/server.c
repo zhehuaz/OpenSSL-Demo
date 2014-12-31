9f51732d7f30a0863c0d1b4f9506cda061266017
zhehua_xiao <zhehuaxiao@gmail.com>
2014-12-31 10:34:06 +0800

    seperate main() function from server.c,encapsulate SSL and socket primitive...but the functions don't support multi-thread well.

--- src/server.c
+++ src/server.c
@@ -4,18 +4,16 @@
 *	Author:Langley Chang
 */
 
-#include <sys/socket.h>
 #include <stdio.h>
-#include <netinet/in.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include "openssl/bio.h"
 #include "openssl/ssl.h"
 #include "openssl/err.h"
 
-#define MYPORT  22087
 #define QUEUE   20
-#define BUFFER_SIZE 1024
 #define EXIT_IF_TRUE(x) if(x)						\
 	do{												\
 		fprintf(stderr, "Check '%s' is true\n", #x);\
@@ -23,9 +21,8 @@
 		exit(2);									\
 	}while(0)
 
-int main()
+SSL_CTX* init_SSL(const char* cert_path,const char* privkey_path)
 {
-	SSL *ssl = NULL;
 	SSL_CTX *ctx = NULL;
 	X509 *client_cert;
 	
@@ -33,58 +30,58 @@ int main()
 	SSL_library_init();
 	OpenSSL_add_all_algorithms();
 	SSL_load_error_strings();
+	if(cert_path == NULL)
+		cert_path = "cacert.pem";
+	if(privkey_path == NULL)
+		privkey_path = "privkey.pem";
 
 	// using SSLv2,v3
 	EXIT_IF_TRUE((ctx = SSL_CTX_new( SSLv23_server_method())) == NULL);
 	// load my certs
-	EXIT_IF_TRUE(SSL_CTX_use_certificate_file(ctx, "cacert.pem",SSL_FILETYPE_PEM) <= 0);
+	EXIT_IF_TRUE(SSL_CTX_use_certificate_file(ctx, cert_path,SSL_FILETYPE_PEM) <= 0);
 	// load my private key
-	EXIT_IF_TRUE(SSL_CTX_use_PrivateKey_file(ctx,"privkey.pem",SSL_FILETYPE_PEM) <= 0);
+	EXIT_IF_TRUE(SSL_CTX_use_PrivateKey_file(ctx, privkey_path,SSL_FILETYPE_PEM) <= 0);
 	// check if private key is correct
 	EXIT_IF_TRUE(!SSL_CTX_check_private_key(ctx));
 
-	// set normal scoket connection
+	return ctx;
+}
+
+//bind & listen
+int set_conn(const int port) 
+{
     int server_sockfd = socket(AF_INET,SOCK_STREAM, 0);
     struct sockaddr_in server_sockaddr;
     server_sockaddr.sin_family = AF_INET;
-    server_sockaddr.sin_port = htons(MYPORT);
+    server_sockaddr.sin_port = htons(port);
     server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     EXIT_IF_TRUE(bind(server_sockfd,(struct sockaddr *)&server_sockaddr,sizeof(server_sockaddr))==-1);
-	printf("Waiting...\n");
-
     EXIT_IF_TRUE(listen(server_sockfd,QUEUE) == -1);
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in client_addr;
-    socklen_t length = sizeof(client_addr);
 
+	return server_sockfd;
+}
+
+//accept
+int accept_s(SSL **ssl,SSL_CTX **ctx,const int server_sockfd,struct sockadd_in* client_addr)
+{
+    socklen_t length = sizeof(client_addr);
     int conn = accept(server_sockfd, (struct sockaddr*)&client_addr, &length);
-    if(conn<0)
-    {
-        perror("connect ERROR");
-        exit(1);
-    }	
-	else
-		printf("User Connected.\n");
+    EXIT_IF_TRUE(conn<0);
 
 	//submit socket connection to SSL
-	EXIT_IF_TRUE((ssl = SSL_new(ctx)) == NULL);
-	SSL_set_fd(ssl,conn);
-	EXIT_IF_TRUE(SSL_accept(ssl) != 1);
+	EXIT_IF_TRUE(((*ssl) = SSL_new(*ctx)) == NULL);
+	SSL_set_fd((*ssl),conn);
+	EXIT_IF_TRUE(SSL_accept(*ssl) != 1);
 
-    while(1)
-    {
-        memset(buffer,0,sizeof(buffer));
-        int len = SSL_read(ssl, buffer, sizeof(buffer));
-        if(!strcmp(buffer,"exit\n") || len == 0)
-            break;
-		printf("%s",buffer);
-        SSL_write(ssl, buffer, len);
-    }
-	printf("User Exit.\n");
-    close(conn);
+	return conn;
+}
+
+//close
+int release(int client_sockfd,int server_sockfd,SSL* ssl,SSL_CTX* ctx)
+{
+    close(client_sockfd);
     close(server_sockfd);
 	SSL_free(ssl);
 	SSL_CTX_free(ctx);
-	
-    return 0;
-}
+}	
+
---
+++ src/srvDemo.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "openssl/bio.h"
+#include "openssl/ssl.h"
+#include "openssl/err.h"
+
+#define PORT 22087
+#define BUFFER_SIZE 1024
+
+int main()
+{
+	struct sockaddr_in client_addr;
+	SSL *ssl;
+	SSL_CTX* ctx = init_SSL(NULL,NULL);
+	int srv_fd = set_conn(PORT);
+	int client_conn = accept_s(&ssl,&ctx,srv_fd,&client_addr);
+	char buffer[BUFFER_SIZE];
+
+	memset(buffer,0,sizeof(buffer));
+	int len = SSL_read(ssl,buffer,sizeof(buffer));
+	printf("%s\n",buffer);
+	SSL_write(ssl, buffer, len);
+	release(client_conn,srv_fd,ssl,ctx);
+}
+
